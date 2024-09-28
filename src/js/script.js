8bdd7979dc9f31b7f96f9a0404115acd1efb9e38
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2024-09-28 18:15:28 +0100

    realistic spheres

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
+
+const hdTexttureURL = new URL(
+  "../img/MR_INT-003_Kitchen_Pierre.hdr",
+  import.meta.url
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 0, 7);
+orbit.update();
+
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1.8;
+
+const loader = new RGBELoader();
+loader.load(hdTexttureURL, (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      roughness: 0,
+      metalness: 0.5,
+      color: 0xffea00,
+    })
+  );
+  scene.add(sphere);
+  sphere.position.x = 1.5;
+
+  const sphere2 = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      roughness: 0,
+      metalness: 0.5,
+      color: 0x00ff00,
+    })
+  );
+  scene.add(sphere2);
+  sphere2.position.x = -1.5;
+});
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
